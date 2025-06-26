e645e4476f5dde573ffed832aaeacfcdfce3779b
ChristianF22 <christianfonseca991@gmail.com>
2025-06-25 22:59:10 -0300

    Deploy do arquivo do back end

---
+++ backend/testefullstack/src/test/java/br/com/testefullstack/testefullstack/TestefullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testefullstack.testefullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestefullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
